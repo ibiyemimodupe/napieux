baee53be9d8396816c46e5e1eabb8078c3119b8f
Ibiyemi Oluwamodupe <ibiyemideborah@gmail.com>
2024-03-31 20:34:50 +0100

    second commit

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
-  type: "loop",
+  loop: true,
   spaceBetween: 1, // Adjust the space between slides
   pagination: {
     el: ".swiper-pagination",
@@ -115,3 +115,30 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+const scrollers = document.querySelectorAll(".scroller");
+
+// If a user hasn't opted in for recuded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
